6365cd82fd39e6d3d7fd1c7c92eb3822f509059f
adiel-filho <adiel@palattine.com.br>
2025-11-19 17:41:24 -0300

    Update products.js

--- api/products.js
+++ api/products.js
@@ -1,6 +1,5 @@
 export default async function handler(req, res) {
 
-  // CORS liberado
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
@@ -12,14 +11,23 @@ export default async function handler(req, res) {
   try {
     const url = "https://cdn5.editmysite.com/app/store/api/v28/editor/users/43097125/sites/223996372125883883/products?page=1&per_page=150&sort_by=created_date&sort_order=desc&include=images,media_files,discounts";
 
-    const response = await fetch(url);
-    const data = await response.json();
-    
-    // A API do Square devolve assim:
-    // [{ data: [ ...produtos... ] }]
-    const rawProducts = data?.[0]?.data || [];
+    const response = await fetch(url, {
+      headers: {
+        "User-Agent": "Mozilla/5.0",
+        "Accept": "application/json",
+      }
+    });
 
-    const products = rawProducts.map(p => {
+    const data = await response.json();
+
+    // 👇 TENTANDO TODOS OS FORMATOS POSSÍVEIS
+    const raw =
+      data?.[0]?.data ||      // formato N8N
+      data?.data?.data ||     // formato HTTP Request antigo
+      data?.data ||           // formato puro
+      [];
+
+    const products = raw.map(p => {
       const image =
         p.images?.data?.[0]?.absolute_url ||
         p.thumbnail?.data?.absolute_url ||
@@ -28,9 +36,9 @@ export default async function handler(req, res) {
       return {
         id: p.id,
         name: p.name,
+        description: p.short_description || "",
         price: p.price?.low_formatted || "",
         image,
-        description: p.short_description || "",
         link: p.absolute_site_link || ""
       };
     });
